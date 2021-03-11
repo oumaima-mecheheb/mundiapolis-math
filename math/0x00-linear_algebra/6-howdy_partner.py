e490a6eaddb47349da7ac173b94de4939d749b32
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-11 09:48:08 +0000

    Update 6-howdy_partner.py

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,12 +1,3 @@
 #!/usr/bin/env python3
-
 def cat_arrays(arr1, arr2):
     return arr1 + arr2
-
-cat_arrays = __import__('6-howdy_partner').cat_arrays
-
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
