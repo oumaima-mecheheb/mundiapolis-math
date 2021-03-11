85e2ac57f7cb20329cdeaa61454447c7121c517d
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-11 09:54:02 +0000

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
 def matrix_shape(matrix):
     type_matrix = type(matrix[0])
     if (type_matrix != list):
         return [len(matrix)]
     else :
-        return [len(matrix)] + matrix_shape(matrix[0])
-
-
+        shape = [len(matrix)] + matrix_shape(matrix[0])
+        return shape
