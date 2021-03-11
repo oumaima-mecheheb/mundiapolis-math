d3b98e2f5242c760e61796f989d9c379579efc46
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-11 09:45:10 +0000

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
-
 def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         return None 
     else :
         sum = [arr1[i] + arr2[i] for i in range(len(arr1))]
         return sum 
-
-add_arrays = __import__('4-line_up').add_arrays
-
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
