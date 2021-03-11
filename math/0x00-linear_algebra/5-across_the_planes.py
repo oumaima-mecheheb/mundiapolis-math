ef83b5b517350b408644f8576819da5847e642ea
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-11 09:46:49 +0000

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
-
 def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2) and len(mat1[0]) != len(mat2[0]):
         return None
     else :
         sum = [[mat1[j][i] + mat2[j][i] for i in range(len(mat1[0]))] for j in range(len(mat1))]
     return sum
-
-add_matrices2D = __import__('5-across_the_planes').add_matrices2D
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
-print(mat1)
-print(mat2)
-#print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
