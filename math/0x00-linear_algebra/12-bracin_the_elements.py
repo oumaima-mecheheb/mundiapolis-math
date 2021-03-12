925d5c42451ff6b79b6d1c3dcb5a8124a19b25a8
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-12 12:52:52 +0000

    Update 12-bracin_the_elements.py

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
-import numpy as np
 def np_elementwise(mat1, mat2):
-    return mat1 + mat2, mat1 - mat2, mat1*mat2, mat1/mat2
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
