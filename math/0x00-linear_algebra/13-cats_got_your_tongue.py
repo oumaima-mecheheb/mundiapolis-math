5c67b327deb5bb96d642c3b478c626c182175edc
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-18 12:10:10 +0000

    Update 13-cats_got_your_tongue.py

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+
 import numpy as np
+
+
 def np_cat(mat1, mat2, axis=0):
-    mat = np.concatenate([mat1, mat2], axis)
-    return mat 
+    mat = np.concatenate ([mat1, mat2], axis)
+    return mat
