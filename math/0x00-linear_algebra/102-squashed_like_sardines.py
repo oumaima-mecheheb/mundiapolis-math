51a2b90e061af9a2b1d36132720e1f28d74f5642
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-18 12:15:41 +0000

    Add files via upload

---
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import time
+
+def shape(matrix):
+    shape = [len(matrix)]
+    while type(matrix[0]) == list:
+        shape.append(len(matrix[0]))
+        matrix = matrix[0]
+    return shape
+
+
+def rec_cat_matrices(mat1, mat2, axis, depth):
+    matrix = []
+    if depth == axis:
+        matrix = mat1 + mat2
+        return matrix
+    for i in range(len(mat1)):
+        matrix.append(rec_cat_matrices(mat1[i], mat2[i], axis, depth + 1))
+    return matrix
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    shape1 = shape(mat1)
+    shape2 = shape(mat2)
+    shape1.pop(axis)
+    shape2.pop(axis)
+    if shape1 != shape2 or axis >= len(shape1) + 1:
+        return None
+    depth = 0
+    return rec_cat_matrices(mat1, mat2, axis, depth)
+
+cat_matrices = __import__('102-squashed_like_sardines').cat_matrices
+
+mat1 = [1, 2, 3]
+mat2 = [4, 5, 6]
+np_mat1 = np.array(mat1)
+np_mat2 = np.array(mat2)
+
+t0 = time.time()
+m = cat_matrices(mat1, mat2)
+t1 = time.time()
+print(t1 - t0)
+print(m)
+t0 = time.time()
+np.concatenate((np_mat1, np_mat2))
+t1 = time.time()
+print(t1 - t0, "\n")
+
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+np_mat1 = np.array(mat1)
+np_mat2 = np.array(mat2)
+
+t0 = time.time()
+m = cat_matrices(mat1, mat2)
+t1 = time.time()
+print(t1 - t0)
+print(m)
+t0 = time.time()
+np.concatenate((np_mat1, np_mat2))
+t1 = time.time()
+print(t1 - t0, "\n")
+
+t0 = time.time()
+m = cat_matrices(mat1, mat2, axis=1)
+t1 = time.time()
+print(t1 - t0)
+print(m)
+t0 = time.time()
+np.concatenate((mat1, mat2), axis=1)
+t1 = time.time()
+print(t1 - t0, "\n")
+
+mat3 = [[[[1, 2, 3, 4], [5, 6, 7, 8]],
+         [[9, 10, 11, 12], [13, 14 ,15, 16]],
+         [[17, 18, 19, 20], [21, 22, 23, 24]]],
+        [[[25, 26, 27, 28], [29, 30, 31, 32]],
+         [[33, 34, 35, 36], [37, 38, 39, 40]],
+         [[41, 42, 43, 44], [45, 46, 47, 48]]]]
+mat4 = [[[[11, 12, 13, 14], [15, 16, 17, 18]],
+         [[19, 110, 111, 112], [113, 114 ,115, 116]],
+         [[117, 118, 119, 120], [121, 122, 123, 124]]],
+        [[[125, 126, 127, 128], [129, 130, 131, 132]],
+         [[133, 134, 135, 136], [137, 138, 139, 140]],
+         [[141, 142, 143, 144], [145, 146, 147, 148]]]]
+mat5 = [[[[11, 12, 13, 14], [15, 16, 17, 18]],
+         [[117, 118, 119, 120], [121, 122, 123, 124]]],
+        [[[125, 126, 127, 128], [129, 130, 131, 132]],
+         [[141, 142, 143, 144], [145, 146, 147, 148]]]]
+np_mat3 = np.array(mat3)
+np_mat4 = np.array(mat4)
+np_mat5 = np.array(mat5)
+
+t0 = time.time()
+m = cat_matrices(mat3, mat4, axis=3)
+t1 = time.time()
+print(t1 - t0)
+print(m)
+t0 = time.time()
+np.concatenate((np_mat3, np_mat4), axis=3)
+t1 = time.time()
+print(t1 - t0, "\n")
+
+t0 = time.time()
+m = cat_matrices(mat3, mat5, axis=1)
+t1 = time.time()
+print(t1 - t0)
+print(m)
+t0 = time.time()
+np.concatenate((np_mat3, np_mat5), axis=1)
+t1 = time.time()
+print(t1 - t0, "\n")
+
+m = cat_matrices(mat2, mat5)
+print(m)
