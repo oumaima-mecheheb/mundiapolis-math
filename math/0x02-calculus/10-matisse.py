cbd2f7551834aa9d823f533c13bb11ad785dcf53
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-04-02 21:33:12 +0000

    Add files via upload

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+'''Calculates the derivate of a polynomial.
+'''
+
+
+def poly_derivative(poly):
+    '''Calculates the derivate of a polynomial.
+    Args:
+        poly: ist of coefficients representing a polynomial.
+    '''
+
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    derivate = []
+    for idx, x in enumerate(poly):
+        if idx - 1 < 0:
+            pass
+        else:
+            derivate.insert(idx - 1, idx * x)
+    if len(derivate) == 0:
+        return [0]
+    else:
+        return derivate
+
+
+poly_derivative = __import__('10-matisse').poly_derivative
+
+poly = [5, 3, 0, 1]
+print(poly_derivative(poly))
