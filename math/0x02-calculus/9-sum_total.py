ab3b494a70ece059f99245854be22e1b81778799
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-04-02 22:28:46 +0100

    Update 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -9,8 +9,3 @@ def summation_i_squared(n):
     if not isinstance(n, int) or n <= 0:
         return None
     return int((n*(n + 1)*(2*n + 1)) / 6)
-
-summation_i_squared = __import__('9-sum_total').summation_i_squared
-
-n = 5
-print(summation_i_squared(n))
